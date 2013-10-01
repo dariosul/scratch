43ad3f111d130d189af4f4f0f716132e261d223f
drusk <drusk@uvic.ca>
2013-10-01 16:10:16 -0700

    Motif form: added text field inputs.

--- motif/dialogs/node_info.c
+++ motif/dialogs/node_info.c
@@ -7,8 +7,13 @@
 
 #include <stdlib.h>
 
+#include <Xm/Form.h>
+#include <Xm/LabelG.h>
 #include <Xm/PushB.h>
 #include <Xm/RowColumn.h>
+#include <Xm/TextF.h>
+
+char *labels[] = { "Index:", "X:", "Y:", "Z:", "Code:" };
 
 int main(int argc, char *argv[]) {
 
@@ -17,6 +22,12 @@ int main(int argc, char *argv[]) {
 	Widget row_column;
 	Widget update_button;
 	Widget close_button;
+	Widget text;
+	Widget form;
+	Widget label;
+	Arg args[8];
+	int i;
+	int n;
 
 	void update_callback(Widget, XtPointer, XtPointer);
 	void close_callback(Widget, XtPointer, XtPointer);
@@ -27,6 +38,35 @@ int main(int argc, char *argv[]) {
 			argv, NULL, sessionShellWidgetClass, NULL);
 
 	row_column = XmCreateRowColumn(root_widget, "MainRowCol", NULL, 0);
+
+	for (i = 0; i < XtNumber(labels); i++) {
+		n = 0;
+		XtSetArg(args[n], XmNfractionBase, 10); n++;
+		XtSetArg(args[n], XmNnavigationType, XmNONE); n++;
+		form = XmCreateForm(row_column, "form", args, n);
+
+		n = 0;
+		XtSetArg(args[n], XmNtopAttachment, XmATTACH_FORM); n++;
+		XtSetArg(args[n], XmNbottomAttachment, XmATTACH_FORM); n++;
+		XtSetArg(args[n], XmNleftAttachment, XmATTACH_FORM); n++;
+		XtSetArg(args[n], XmNrightAttachment, XmATTACH_POSITION); n++;
+		XtSetArg(args[n], XmNrightPosition, 3); n++;
+		XtSetArg(args[n], XmNalignment, XmALIGNMENT_END); n++;
+		XtSetArg(args[n], XmNnavigationType, XmNONE); n++;
+		label = XmCreateLabelGadget(form, labels[i], args, n);
+		XtManageChild(label);
+
+		n = 0;
+		XtSetArg(args[n], XmNtraversalOn, True); n++;
+		XtSetArg(args[n], XmNleftAttachment, XmATTACH_POSITION); n++;
+		XtSetArg(args[n], XmNleftPosition, 4); n++;
+		XtSetArg(args[n], XmNrightAttachment, XmATTACH_FORM); n++;
+		XtSetArg(args[n], XmNnavigationType, XmTAB_GROUP); n++;
+		text = XmCreateTextField(form, "text", args, n);
+		XtManageChild(text);
+
+		XtManageChild(form);
+	}
 
 	update_button = XmCreatePushButton(row_column, "Update", NULL, 0);
 	XtAddCallback(update_button, XmNactivateCallback, update_callback, NULL);
