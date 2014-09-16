5c10616f220678ad2e5c52de26bc2d064f3055a2
drusk <drusk@uvic.ca>
2014-09-16 16:52:04 -0700

    Redirect after post.

--- django-tdd/ch5/superlists/lists/tests.py
+++ django-tdd/ch5/superlists/lists/tests.py
@@ -25,14 +25,20 @@ class HomePageTest(TestCase):
         item_text = "A new list item"
         request.POST["item_text"] = item_text
 
-        expected_html = render_to_string(
-            "home.html",
-            {"new_item_text": item_text}
-        )
-
         response = home_page(request)
 
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, item_text)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response["location"], "/")
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+
+        self.assertEqual(Item.objects.count(), 0)
 
 
 class ItemModelTest(TestCase):
